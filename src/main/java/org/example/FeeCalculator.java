6690729217cf2ab6e0946c1c938dd7e18c037a46
Candice Dutournier <cdutourni001@iutbayonne.univ-pau.fr>
2026-03-17 16:55:29 +0100

    refactoring etape 2

--- src/main/java/org/example/FeeCalculator.java
+++ src/main/java/org/example/FeeCalculator.java
@@ -1,39 +1,20 @@
 package org.example;
 
 public class FeeCalculator {
-    private static final double CHILD_PRICE_BASE = 100;
-    private static final double ADULT_PRICE_BASE = 100;
 
     public static double calculateFee(Visitor visitor, TicketType ticketType) {
         double fee = 0;
 
         // calculate price for adults
         if (visitor.age > 14) {
-            fee = calculateAdultFee(ticketType);
+            fee = new AdultFeeStrategy().calculateAdultFee(ticketType);
         }
 
         // calculate price for children
         if (visitor.age <= 14) {
-            fee = calculateChildFee(ticketType);
+            fee = new ChildFeeStrategy().calculateChildFee(ticketType);
         }
         return fee;
     }
 
-    private static double calculateChildFee(TicketType ticketType) {
-        if (TicketType.HALF_DAY == ticketType) {
-            return CHILD_PRICE_BASE * 0.2;
-        } else if (TicketType.FULL_DAY == ticketType) {
-            return CHILD_PRICE_BASE * 0.5;
-        }
-        throw (new IllegalArgumentException("TicketType not supported")) ;
-    }
-
-    private static double calculateAdultFee(TicketType ticketType) {
-        if (TicketType.HALF_DAY == ticketType) {
-            return ADULT_PRICE_BASE * 0.6;
-        } else if (TicketType.FULL_DAY == ticketType) {
-            return ADULT_PRICE_BASE * 1.2;
-        }
-        throw (new IllegalArgumentException("TicketType not supported")) ;
-    }
 }
